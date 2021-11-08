70b73297b32dcd465c5c56ac2e769c1d911498b8
Hristo Slavchev <84782275+Iceto04@users.noreply.github.com>
2021-11-08 13:33:59 +0200

    Add school projects

---
+++ School2020-2021/Pass-Length/Pass-Length.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи парол,която е над 8 символа:");
+            var pass = Console.ReadLine();
+            var a = false;
+            while (pass.Length > 8)
+            {
+                a = true;
+                break;
+            }
+            if (a == true)
+            {
+                Console.WriteLine("Добре дошли");
+            }
+            else
+            {
+                Console.WriteLine("Грешна парола");
+            }
+        }
+    }
+}
