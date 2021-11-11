34ff973c2d1f65d1dc087a95a0f80f4e40dca59f
Iceto04 <izetohr@gmail.com>
2021-11-11 22:46:19 +0200

    add task solution

---
+++ JS Advanced/03. Objects and Composition/Lab/01.cityRecond.js
@@ -0,0 +1,15 @@
+function solve(name, population, treasury) {
+    const city = {
+        name: name,
+        population: population,
+        treasury: treasury
+    };
+
+    return city;
+}
+
+console.log(solve(
+    'Tortuga',
+    7000,
+    15000
+));
