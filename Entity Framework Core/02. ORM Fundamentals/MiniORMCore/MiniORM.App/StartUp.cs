e4d8940288d59a7e6d8d07f2d44bb77c05c0f393
Iceto04 <izetohr@gmail.com>
2021-12-27 19:29:15 +0200

    add MiniORMCore Test

---
+++ Entity Framework Core/02. ORM Fundamentals/MiniORMCore/MiniORM.App/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = "Server=.;Database=MiniORM;Integrated Security=true;TrustServerCertificate=true";
+
+            SoftUniDbContext dbContext = new SoftUniDbContext(connectionString);
+
+            dbContext.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = dbContext.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            Employee employee = dbContext.Employees.Last();
+            employee.FirstName = "Modified";
+
+            dbContext.SaveChanges();
+        }
+    }
+}
