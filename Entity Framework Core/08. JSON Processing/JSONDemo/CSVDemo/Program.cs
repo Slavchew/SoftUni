15b4063fe0c23bbca44299e80202b9ae33e24809
Iceto04 <izetohr@gmail.com>
2022-01-05 21:26:18 +0200

    add JSON and CSV explanation

---
+++ Entity Framework Core/08. JSON Processing/JSONDemo/CSVDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using System.Collections.Generic;
+using CsvHelper;
+using System.Text;
+
+namespace CSVDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // From CSV to List
+            using CsvReader reader = new CsvReader(new StreamReader("Cars.csv"), CultureInfo.InvariantCulture);
+            List<Car> cars = reader.GetRecords<Car>().ToList();
+
+            // from List to CSV
+            var carsList = new List<Car>
+            {
+                new Car { Year = 2006, Model = "320", Make = "BMW", Description = "Biva kolata", Price = 8500M },
+                new Car { Year = 2020, Model = "Model X", Make = "Tesla", Description = "AI Car", Price = 500000M },
+                new Car { Year = 2004, Model = "Golf 3", Make = "VW", Description = "Best car", Price = 100000M }
+            };
+
+            using CsvWriter writer = new CsvWriter(new StreamWriter("MyCars.csv"), CultureInfo.InvariantCulture);
+            writer.WriteRecords(carsList);
+        }
+    }
+
+    class Car
+    {
+        public int Year { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ Entity Framework Core/08. JSON Processing/JSONDemo/JsonDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Xml;
+
+namespace JsonDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherForecast weather = new WeatherForecast();
+            Forecasts forecast = new Forecasts() 
+            { 
+                AddiotionData = new Tuple<int, string>(123, "Niki"),
+                Forecast = new List<WeatherForecast>() 
+                { 
+                    new WeatherForecast(), 
+                    new WeatherForecast(), 
+                    new WeatherForecast()
+                }
+            };
+
+            /* System.Text.Json
+            string json = JsonSerializer.Serialize(weather, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            });
+            Console.WriteLine(json);
+            */
+
+
+            /* Json.Net
+            var json = File.ReadAllText("weather.json");
+            JObject jObject = JObject.Parse(json);
+            foreach (var item in jObject["connectionStrings"]
+                .Where(x => x.ToString().Contains("Server")))
+            {
+                Console.WriteLine(item);
+            }
+            */
+
+
+            // XML-to-JSON
+
+            string xml = @"<?xml version='1.0' standalone='no'?> 
+                         <root> 
+                            <person id='1'> 
+                                <name>Alan</name> 
+                                <url>www.google.com</url> 
+                            </person> 
+                            <person id='2'> 
+                                <name>Louis</name> 
+                                <url>www.yahoo.com</url> 
+                            </person> 
+                        </root>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            string jsonText = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(jsonText);
+
+        }
+    }
+
+    class WeatherForecast
+    {
+        public decimal LongNameOfThisDecimalProperty { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public List<int> TemperaturesC { get; set; } = new List<int> {30, 25, 36};
+
+        public string Summary { get; set; } = "Hot summer day";
+    }
+    
+    class Forecasts
+    {
+        public Tuple<int, string> AddiotionData { get; set; }
+
+        public List<WeatherForecast> Forecast { get; set; }
+    }
+}
