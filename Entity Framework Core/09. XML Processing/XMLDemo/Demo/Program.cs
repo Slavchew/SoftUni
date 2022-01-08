d73fe5aa0958e91969b0ca3d1131805b4cea6deb
Iceto04 <izetohr@gmail.com>
2022-01-08 11:36:18 +0200

    add XML demo

---
+++ Entity Framework Core/09. XML Processing/XMLDemo/Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xmlDocument = XDocument.Load("Planes.xml");
+            Console.WriteLine(xmlDocument.Root.Elements()
+                .FirstOrDefault(x => x.Element("color").Value == "Blue"));
+
+        }
+    }
+}
