3c3e9cc4a8fb40825eebd1b536026ec92cb10a62
Iceto04 <izetohr@gmail.com>
2022-01-09 12:49:49 +0200

    Create EF data models

---
+++ Entity Framework Core/10. Best Practices And Architecture/RealEstates/RealEstates.Models/RealEstatePropertyTag.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstates.Models
+{
+    public class RealEstatePropertyTag
+    {
+        public int PropertyId { get; set; }
+
+        public virtual RealEstateProperty Property { get; set; }
+
+        public int TagId { get; set; }
+
+        public virtual Tag Tag { get; set; }
+    }
+}
