27e68b36512771d244837a2c5b11369f83f4ce26
Iceto04 <izetohr@gmail.com>
2022-01-09 11:40:21 +0200

    Create project structure

---
+++ Entity Framework Core/10. Best Practices And Architecture/RealEstates/RealEstates.Importer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RealEstates.Importer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
