34e13f67ce4e9f0315d1b0c65ac216044c2a4a76
Iceto04 <izetohr@gmail.com>
2022-01-09 18:01:25 +0200

    Implement services and RealEstates.Importer and Import over 22000 records in database

--- Entity Framework Core/10. Best Practices And Architecture/RealEstates/RealEstates.ConsoleApplication/Program.cs
+++ Entity Framework Core/10. Best Practices And Architecture/RealEstates/RealEstates.ConsoleApplication/Program.cs
@@ -1,5 +1,8 @@
-﻿using RealEstates.Data;
-using System;
+﻿using System;
+using System.Text;
+using RealEstates.Data;
+using RealEstates.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace RealEstates.ConsoleApplication
 {
@@ -7,8 +10,32 @@ namespace RealEstates.ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             var db = new RealEstateDbContext();
-            db.Database.EnsureCreated();
+            db.Database.Migrate();
+
+            
+
+            IPropertiesService propertiesService = new PropertiesService(db);
+            Console.Write("Min price: ");
+            int minPrice = int.Parse(Console.ReadLine());
+            Console.Write("Max price: ");
+            int maxPrice = int.Parse(Console.ReadLine());
+            var properties = propertiesService.SearchByPrice(minPrice, maxPrice);
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"{property.District}, fl. {property.Floor}, {property.Size} ㎡, {property.Year}, {property.Price}€, {property.PropertyType}, {property.BuildingType}");
+            }
+
+            /*
+            IDistrictService districtService = new DistrictService(db);
+            var districts = districtService.GetTopDistrictsByAveragePrice();
+            foreach (var district in districts)
+            {
+                Console.WriteLine($"{district.Name} => Price: {district.AveragePrice:f2} ({district.MinPrice}-{district.MaxPrice}) => {district.PropertiesCount} properties");
+            }
+            */
         }
     }
 }
---
+++ Entity Framework Core/10. Best Practices And Architecture/RealEstates/RealEstates.Services/DistrictService.cs
@@ -0,0 +1,49 @@
+﻿using RealEstates.Data;
+using RealEstates.Models;
+using RealEstates.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RealEstates.Services
+{
+    public class DistrictService : IDistrictService
+    {
+        private RealEstateDbContext db;
+        public DistrictService(RealEstateDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<DistrictViewModel> GetTopDistrictsByAveragePrice(int count = 10)
+        {
+            return db.Districts
+                .OrderByDescending(x => x.Properties.Average(x => (double)x.Price / x.Size))
+                .Select(MapToDistrictViewModel())
+                .Take(count)
+                .ToList();
+        }
+
+        public IEnumerable<DistrictViewModel> GetTopDistrictsByNumberOfProperties(int count = 10)
+        {
+            return db.Districts
+                .OrderByDescending(x => x.Properties.Count())
+                .Select(MapToDistrictViewModel())
+                .Take(count)
+                .ToList();
+        }
+
+        private static Expression<Func<District, DistrictViewModel>> MapToDistrictViewModel()
+        {
+            return x => new DistrictViewModel
+            {
+                Name = x.Name,
+                AveragePrice = x.Properties.Average(x => (double)x.Price / x.Size),
+                MinPrice = x.Properties.Min(x => x.Price),
+                MaxPrice = x.Properties.Max(x => x.Price),
+                PropertiesCount = x.Properties.Count()
+            };
+        }
+    }
+}
