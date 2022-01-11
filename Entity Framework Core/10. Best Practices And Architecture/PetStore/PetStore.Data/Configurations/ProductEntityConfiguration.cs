8d91c949624ada1ff5957c3d9b0fde64329832da
Iceto04 <izetohr@gmail.com>
2022-01-11 20:40:52 +0200

    SetUp Product name to be alternate key

---
+++ Entity Framework Core/10. Best Practices And Architecture/PetStore/PetStore.Data/Configurations/ProductEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Data.Configurations
+{
+    public class ProductEntityConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasAlternateKey(p => p.Name);
+        }
+    }
+}
--- Entity Framework Core/10. Best Practices And Architecture/PetStore/PetStore.Data/Migrations/20220111183904_SetUpProductNameAlternateKey.Designer.cs
+++ Entity Framework Core/10. Best Practices And Architecture/PetStore/PetStore.Data/Migrations/20220111183904_SetUpProductNameAlternateKey.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetStore.Data;
 
 namespace PetStore.Data.Migrations
 {
     [DbContext(typeof(PetStoreDbContext))]
-    partial class PetStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220111183904_SetUpProductNameAlternateKey")]
+    partial class SetUpProductNameAlternateKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
