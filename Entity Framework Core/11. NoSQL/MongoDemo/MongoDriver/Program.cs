fcfea1fc1b8b375fb95ee85f84e08732656ded82
Iceto04 <izetohr@gmail.com>
2022-01-16 20:33:25 +0200

    Add NoSQL lecture demo with MongoDB

---
+++ Entity Framework Core/11. NoSQL/MongoDemo/MongoDriver/Program.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace MongoDriver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://127.0.0.1:27017");
+            var datebase = client.GetDatabase("Softuni");
+            var collection = datebase.GetCollection<BsonDocument>("Students");
+            var student = new BsonDocument
+            {
+                { "name", "peshoStudent" }
+            };
+            collection.InsertOne(student);
+
+            var allStudents = collection.Find<BsonDocument>(new BsonDocument()).ToList();
+
+            foreach (var item in allStudents)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
