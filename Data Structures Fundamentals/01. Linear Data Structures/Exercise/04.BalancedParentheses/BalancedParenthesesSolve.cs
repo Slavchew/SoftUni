5814004b8ae73e299d9f2421b4a3fb2afb6b1858
Iceto04 <izetohr@gmail.com>
2022-01-29 00:12:20 +0200

    Add simple solution for Balanced Brackets problem

---
+++ Data Structures Fundamentals/01. Linear Data Structures/Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,52 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in parentheses)
+            {
+                if (c == '{' || c == '[' || c == '(')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    if (c == '}')
+                    {
+                        if (stack.Pop() != '{')
+                        {
+                            return false;
+                        }
+                    }
+                    else if (c == ']')
+                    {
+                        if (stack.Pop() != '[')
+                        {
+                            return false;
+                        }
+                    }
+                    else if (c == ')')
+                    {
+                        if (stack.Pop() != '(')
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
