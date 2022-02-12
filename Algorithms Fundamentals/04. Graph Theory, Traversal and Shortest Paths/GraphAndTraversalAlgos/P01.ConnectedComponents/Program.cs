2be2474daff243455a62aecf37891f01c74bb1b4
Iceto04 <izetohr@gmail.com>
2022-02-12 23:30:23 +0200

    Add graph problems

---
+++ Algorithms Fundamentals/04. Graph Theory, Traversal and Shortest Paths/GraphAndTraversalAlgos/P01.ConnectedComponents/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.ConnectedComponents
+{
+    internal class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[n];
+            visited = new bool[n];
+
+            for (int node = 0; node < n; node++)
+            {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    graph[node] = new List<int>();
+                    continue;
+                }
+
+                var children = line
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+                graph[node] = new List<int>(children);
+            }
+
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                var component = new List<int>();
+                DFS(node, component);
+                Console.WriteLine($"Connected component: {string.Join(" ", component)}");
+            }
+        }
+
+        private static void DFS(int node, List<int> component)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                DFS(child, component);
+            }
+
+            component.Add(node);
+        }
+    }
+}
