4c0bf452fcd0df7bb387c27893645f1b98146a2a
Iceto04 <izetohr@gmail.com>
2022-02-13 01:42:51 +0200

    Add Graph algorithms Implementations

---
+++ Algorithms Fundamentals/04. Graph Theory, Traversal and Shortest Paths/GraphAndTraversalAlgos/P02.TopologicalSorting/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.TopologicalSorting
+{
+    internal class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static Dictionary<string, int> dependencies;
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(n);
+            dependencies = GetPredecessorCount();
+
+            var sorted = TopologicalSorting();
+
+            if (sorted == null)
+            {
+                Console.WriteLine("Invalid topological sorting");
+            }
+            else
+            {
+                Console.WriteLine($"Topological sorting: {string.Join(", ", sorted)}");
+            }
+        }
+
+        private static List<string> TopologicalSorting()
+        {
+            var sorted = new List<string>();
+
+            while (dependencies.Count > 0)
+            {
+                var nodeToRemove = dependencies.FirstOrDefault(x => x.Value == 0);
+
+                if (string.IsNullOrEmpty(nodeToRemove.Key))
+                {
+                    break;
+                }
+
+                var node = nodeToRemove.Key;
+                var children = graph[node];
+
+                sorted.Add(node);
+
+                foreach (var child in children)
+                {
+                    dependencies[child]--;
+                }
+
+                dependencies.Remove(node);
+            }
+
+            if (dependencies.Count > 0)
+            {
+                return null;
+            }
+
+            return sorted;
+        }
+
+        private static Dictionary<string, int> GetPredecessorCount()
+        {
+            var result = new Dictionary<string, int>();
+
+            foreach (var node in graph)
+            {
+                var key = node.Key;
+                var children = node.Value;
+
+                if (!result.ContainsKey(key))
+                {
+                    result.Add(key, 0);
+                }
+
+                foreach (var child in children)
+                {
+                    if (!result.ContainsKey(child))
+                    {
+                        result.Add(child, 0);
+                    }
+
+                    result[child]++;
+                }
+            }
+
+            return result;
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var graphResult = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+
+                var node = line[0];
+
+                if (node.Contains(" ->"))
+                {
+                    node = node.Replace(" ->", "");
+                }
+
+                if (line.Length == 1)
+                {
+                    graphResult[node] = new List<string>();
+                }
+                else
+                {
+                    var childern = line[1]
+                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+
+                    graphResult[node] = new List<string>(childern);
+                }
+            }
+
+            return graphResult;
+        }
+    }
+}
---
+++ Algorithms Fundamentals/04. Graph Theory, Traversal and Shortest Paths/GraphAndTraversalAlgos/P03.ShortestPath/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.ShortestPath
+{
+    internal class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+        private static int[] parents;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var e = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(n, e);
+            visited = new bool[graph.Length];
+            parents = new int[graph.Length];
+            Array.Fill(parents, -1);
+
+            var startNode = int.Parse(Console.ReadLine());
+            var endNode = int.Parse(Console.ReadLine());
+
+            BFS(startNode, endNode);
+        }
+
+        private static void BFS(int source, int destination)
+        {
+            if (visited[source])
+            {
+                return;
+            }
+
+            var queue = new Queue<int>();
+            queue.Enqueue(source);
+
+            visited[source] = true;
+
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node == destination)
+                {
+                    var path = ReconstructPath(destination);
+                    Console.WriteLine($"Shortest path length is: {path.Count - 1}");
+                    Console.WriteLine(string.Join(" ", path));
+
+                    return;
+                }
+
+                foreach (var child in graph[node])
+                {
+                    if (!visited[child])
+                    {
+                        parents[child] = node;
+                        queue.Enqueue(child);
+                        visited[child] = true;
+                    }
+                }
+            }
+        }
+
+        private static Stack<int> ReconstructPath(int destination)
+        {
+            var path = new Stack<int>();
+            var index = destination;
+
+            while (index != -1)
+            {
+                path.Push(index);
+                index = parents[index];
+            }
+
+            return path;
+        }
+
+        private static List<int>[] ReadGraph(int n, int e)
+        {
+            var graphResult = new List<int>[n + 1];
+
+            for (int i = 0; i < graphResult.Length; i++)
+            {
+                graphResult[i] = new List<int>();
+            }
+
+            for (int i = 1; i <= e; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var from = line[0];
+                var to = line[1];
+
+                graphResult[from].Add(to);
+            }
+
+            return graphResult;
+        }
+    }
+}
