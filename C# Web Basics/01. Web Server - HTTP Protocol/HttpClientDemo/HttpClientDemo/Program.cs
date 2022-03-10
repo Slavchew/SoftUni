d707f5ea297efde90a01545f8dd1b9ab6ff8d0ec
Iceto04 <izetohr@gmail.com>
2022-03-10 14:52:32 +0200

    Add my custom HTTP Client implementation

---
+++ C# Web Basics/01. Web Server - HTTP Protocol/HttpClientDemo/HttpClientDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo
+{
+    internal class Program
+    {
+        const string NewLine = "\r\n";
+
+        static async Task Main(string[] args)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 12345);
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                using var stream = client.GetStream();
+
+                byte[] buffer = new byte[1000000];
+                var length = stream.Read(buffer, 0, buffer.Length);
+
+                string requestString = Encoding.UTF8.GetString(buffer, 0, length);
+                Console.WriteLine(requestString);
+
+                string html = $"<h1>Hello from IcetoServer {DateTime.Now}<h1>" +
+                    $"<form action=/tweet method=post><input name=username /><input name=password />" +
+                    $"<input type=submit /></form>";
+
+                string response = "HTTP/1.1 200 OK" + NewLine +
+                    "Server: IcetoServer 2022" + NewLine +
+                    // "Location: https://www.google.com" + NewLine +
+                    "Content-Type: text/html; charset=utf-8" + NewLine +
+                    // "Content-Disposition: attachment; filename=text.txt" + NewLine +
+                    "Content-Length: " + html.Length + NewLine +
+                    NewLine +
+                    html +
+                    NewLine;
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseBytes);
+
+                Console.WriteLine(new string('=', 70));
+            }
+        }
+
+        public static async Task ReadData()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string url = "https://softuni.bg/curriculum";
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(string.Join(Environment.NewLine,
+                response.Headers.Select(x => x.Key + ": " + x.Value.First())));
+
+            // var html = await httpClient.GetStringAsync(url);
+            // Console.WriteLine(html);
+        }
+    }
+}
