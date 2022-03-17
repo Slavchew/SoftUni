1a2303dd2fd2981e21786b21dc7f4d52f50cd67e
Iceto04 <izetohr@gmail.com>
2022-03-17 10:24:14 +0200

    Changes after Async lecture

--- C# Web Basics/01. Web Server - HTTP Protocol/HttpClientDemo/HttpClientDemo/Program.cs
+++ C# Web Basics/01. Web Server - HTTP Protocol/HttpClientDemo/HttpClientDemo/Program.cs
@@ -20,33 +20,69 @@ namespace HttpClientDemo
             while (true)
             {
                 var client = tcpListener.AcceptTcpClient();
-                using var stream = client.GetStream();
-
-                byte[] buffer = new byte[1000000];
-                var length = stream.Read(buffer, 0, buffer.Length);
-
-                string requestString = Encoding.UTF8.GetString(buffer, 0, length);
-                Console.WriteLine(requestString);
-
-                string html = $"<h1>Hello from IcetoServer {DateTime.Now}<h1>" +
-                    $"<form action=/tweet method=post><input name=username /><input name=password />" +
-                    $"<input type=submit /></form>";
-
-                string response = "HTTP/1.1 200 OK" + NewLine +
-                    "Server: IcetoServer 2022" + NewLine +
-                    // "Location: https://www.google.com" + NewLine +
-                    "Content-Type: text/html; charset=utf-8" + NewLine +
-                    // "Content-Disposition: attachment; filename=text.txt" + NewLine +
-                    "Content-Length: " + html.Length + NewLine +
-                    NewLine +
-                    html +
-                    NewLine;
-
-                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                stream.Write(responseBytes);
-
-                Console.WriteLine(new string('=', 70));
+                ProcessClientAsync(client);
+                // ProcessClient(client);
             }
+        }
+
+        public static async Task ProcessClientAsync(TcpClient client)
+        {
+            using var stream = client.GetStream();
+
+            byte[] buffer = new byte[1000000];
+            var length = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+            string requestString = Encoding.UTF8.GetString(buffer, 0, length);
+            Console.WriteLine(requestString);
+
+            string html = $"<h1>Hello from IcetoServer {DateTime.Now}<h1>" +
+                $"<form action=/tweet method=post><input name=username /><input name=password />" +
+                $"<input type=submit /></form>";
+
+            string response = "HTTP/1.1 200 OK" + NewLine +
+                "Server: IcetoServer 2022" + NewLine +
+                // "Location: https://www.google.com" + NewLine +
+                "Content-Type: text/html; charset=utf-8" + NewLine +
+                // "Content-Disposition: attachment; filename=text.txt" + NewLine +
+                "Content-Length: " + html.Length + NewLine +
+                NewLine +
+                html +
+                NewLine;
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            await stream.WriteAsync(responseBytes);
+
+            Console.WriteLine(new string('=', 70));
+        }
+
+        public static void ProcessClient(TcpClient client)
+        {
+            using var stream = client.GetStream();
+
+            byte[] buffer = new byte[1000000];
+            var length = stream.Read(buffer, 0, buffer.Length);
+
+            string requestString = Encoding.UTF8.GetString(buffer, 0, length);
+            Console.WriteLine(requestString);
+
+            string html = $"<h1>Hello from IcetoServer {DateTime.Now}<h1>" +
+                $"<form action=/tweet method=post><input name=username /><input name=password />" +
+                $"<input type=submit /></form>";
+
+            string response = "HTTP/1.1 200 OK" + NewLine +
+                "Server: IcetoServer 2022" + NewLine +
+                // "Location: https://www.google.com" + NewLine +
+                "Content-Type: text/html; charset=utf-8" + NewLine +
+                // "Content-Disposition: attachment; filename=text.txt" + NewLine +
+                "Content-Length: " + html.Length + NewLine +
+                NewLine +
+                html +
+                NewLine;
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseBytes);
+
+            Console.WriteLine(new string('=', 70));
         }
 
         public static async Task ReadData()
