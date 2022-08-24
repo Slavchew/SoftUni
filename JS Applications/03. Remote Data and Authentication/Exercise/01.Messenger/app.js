0c5edcf3344c3947793fae9d42d0ed308e308566
Iceto04 <izetohr@gmail.com>
2022-08-24 22:54:45 +0300

    Add problems(1, 2) from renote data and auth lecture

--- JS Applications/03. Remote Data and Authentication/Exercise/01.Messenger/app.js
+++ JS Applications/03. Remote Data and Authentication/Exercise/01.Messenger/app.js
@@ -1,5 +1,46 @@
+const nameInput = document.querySelector('[name=author]');
+const contentInput = document.querySelector('[name=content]');
+
+const createBtn = document.getElementById('submit');
+const loadDataBtn = document.getElementById('refresh');
+
+const dataDiv = document.getElementById('messages');
+
 function attachEvents() {
-    console.log('TODO...');
+    createBtn.addEventListener('click', createMessage);
+
+    loadDataBtn.addEventListener('click', loadMessages)
+}
+
+function createMessage() {
+    const url = 'http://localhost:3030/jsonstore/messenger';
+
+    const body = {
+        author: nameInput.value.trim(),
+        content: contentInput.value.trim(),
+    };
+
+    const options = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+    };
+
+    fetch(url, options);
+}
+
+async function loadMessages() {
+    const url = 'http://localhost:3030/jsonstore/messenger';
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    let messagesText = [];
+    for (const {author, content} of Object.values(data)) {
+        messagesText.push(`${author}: ${content}`);
+    }
+
+    dataDiv.textContent = messagesText.join('\n');
 }
 
 attachEvents();
--- JS Applications/03. Remote Data and Authentication/Exercise/02.Phonebook/app.js
+++ JS Applications/03. Remote Data and Authentication/Exercise/02.Phonebook/app.js
@@ -1,5 +1,40 @@
+const phonesUl = document.getElementById('phonebook');
+const loadPhonesBtn = document.getElementById('btnLoad');
+
 function attachEvents() {
-    console.log('TODO...');
+    loadPhonesBtn.addEventListener('click', loadPhones)
+}
+
+async function loadPhones() {
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    for (const phoneObj of Object.values(data)) {
+        const li = document.createElement('li');
+        li.textContent = `${phoneObj.person}: ${phoneObj.phone}`;
+        
+        const deleteBtn = document.createElement('btn');
+        deleteBtn.addEventListener('click', deletePhone(event, phoneObj._id));
+
+        li.appendChild(deleteBtn);
+        phonesUl.appendChild(li);
+    }
+
+
+}
+
+function deletePhone(e, id) {
+    e.preventDefault();
+
+    const url = 'http://localhost:3030/jsonstore/phonebook/' + id;
+
+    const options = {
+        method: 'DELETE',
+    }
+
+    fetch(url, options)
 }
 
 attachEvents();
